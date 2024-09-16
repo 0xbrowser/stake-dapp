14f8424e194d9bbd00576ecc7e67409464ce9dc5
0xbrowser <gakiyuikawai@gmail.com>
2024-09-16 11:47:13 +0800

    fix: remove

--- app/stake/page.tsx
+++ app/stake/page.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { Stake } from '@/components/Stake/Stake';
-import { BaseLayout } from '@/components/PageLayout/BaseLayout';
 
 export default function StakePage() {
   return (
